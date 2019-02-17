aac67a459bd7bb750915e7d2c3f6889c9e433551
cafc1234 <camilofique@hotmail.com>
2019-02-16 22:33:11 -0500

    Modal "Arma tu vaca" added and service added
    The modal for the "Arma tu vaca" is created, the component is added, and the action is added to the button in the Tab 2 "crear" in the card of Arma tu vaca.
    Also for the modal "Plan familiar" is added a service to show the possibly persons that can be invited

--- el_desparche/src/app/tab2/tab2.page.ts
+++ el_desparche/src/app/tab2/tab2.page.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
-// Se importa el servicio
-import { UserServiceService } from '../services/user-service.service';
 import { ModalController } from '@ionic/angular';
 import { ModalFamiliarPage } from '../modal-familiar/modal-familiar.page';
+import { ModalVacaPage } from '../modal-vaca/modal-vaca.page';
+
 
 
 @Component({
@@ -12,7 +12,7 @@ import { ModalFamiliarPage } from '../modal-familiar/modal-familiar.page';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  constructor(public userService: UserServiceService,public modalController: ModalController){
+  constructor(public modalController: ModalController){
   
   }
   //Se encarga de mostrar el modal familiar
@@ -23,28 +23,16 @@ export class Tab2Page {
     });
     return await modal.present();
   }
+
+  async presentModalVaca() {
+    const modal = await this.modalController.create({
+      component: ModalVacaPage,
+      componentProps: { value: 123 }
+    });
+    return await modal.present();
+  }
 }
 
-  /*
-  //Se define arreglo de tipo any
-  users: any[] = [];
-
-  //Se carga clase del import userService
  
-
-  //Cuando cargue Ionic
-  loadUser(){
-    this.userService.getUsers().subscribe(
-    (data) => { // Success
-    this.users = data['results'];
-    },
-    (error) =>{
-    console.error(error);
-    }
-    )
-    
-  }
-  */
-
     
 
