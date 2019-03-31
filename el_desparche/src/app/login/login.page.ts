f0a15ac7e256355aa1308866ae42bd3c3fbcfae8
cafc1234 <camilofique@hotmail.com>
2019-03-31 13:18:05 -0500

    Redirect after login successful
    If a user is already logged in the app will be redirected to the home page, also,also, the old code for the login is deleted

--- el_desparche/src/app/login/login.page.ts
+++ el_desparche/src/app/login/login.page.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { ToastController } from '@ionic/angular';
-import { Validators } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 
@@ -12,76 +9,19 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private storage: Storage, public toastController: ToastController, public afAuth: AngularFireAuth) { }
+  constructor( public afAuth: AngularFireAuth) { }
 
-  inicio = {
-    'usuario': "",
-    'clave': ""
-  };
+
 
   ngOnInit() {
-     
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        window.location.href = "nav";
+      }
+    }); 
   }
 
-  successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult) {
-    ...
-}
 
-  async toastError() {
-    const toast = await this.toastController.create({
-      message: 'Los campos no pueden estar vacios.',
-      duration: 2000,
-      color: 'danger'
-    });
-    toast.present();
-  }
-  async toastErrorUsuario() {
-    const toast = await this.toastController.create({
-      message: 'El usuario o clave son incorrectos.',
-      duration: 2000,
-      color: 'danger'
-    });
-    toast.present();
-  }
-
-  obtenerUsuario(validarUsuario) {
-    var valor = "";
-    this.storage.get('usuario').then(function (val) {
-      valor = val;
-      validarUsuario(valor);
-    });
-
-  }
-
-  obtenerClave(validarClave) {
-    var valor = "";
-    this.storage.get('clave').then(function (val) {
-      valor = val;
-      validarClave(valor);
-    });
-  }
-
-  confirmarIngreso() {
-    if (this.inicio.usuario == "" || this.inicio.clave == "") {
-      this.toastError();
-    } else {
-      var inside=this;
-
-      this.obtenerUsuario(function (usuario) {
-        if (inside.inicio.usuario == usuario) {
-          inside.obtenerClave(function (clave) {
-            if (inside.inicio.clave == clave) {
-              window.location.href = "nav";
-            } else {
-              inside.toastErrorUsuario();
-            }
-          });
-        } else {
-          inside.toastErrorUsuario();
-        }
-      })
-    }
-  }
 
 
 
