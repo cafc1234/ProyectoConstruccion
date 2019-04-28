59a98dd90a69ab3fa0ba9b3b60ed00eef5f77b75
cafc1234 <camilofique@hotmail.com>
2019-04-27 21:41:19 -0500

    Logout function
    Now an user can log out from the app

--- el_desparche/src/app/cerrar-sesion/cerrar-sesion.page.ts
+++ el_desparche/src/app/cerrar-sesion/cerrar-sesion.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-cerrar-sesion',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CerrarSesionPage implements OnInit {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  cerrarSesion(){
+    this.afAuth.auth.signOut().then(()=>{
+      window.location.href = "/";
+    })
   }
 
 }
