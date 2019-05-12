01b0ea3b17196d3eb21a650703bbb43a2c65d126
cafc1234 <camilofique@hotmail.com>
2019-05-11 21:23:43 -0500

    Apk Signed
    Apk signed

--- el_desparche/src/environments/environment.ts
+++ el_desparche/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase:{
     apiKey: "AIzaSyCXWyZegNWDDlHjX_FlYNL4Abxvfl1h-pE",
     authDomain: "proyectoconstruccion.firebaseapp.com",
