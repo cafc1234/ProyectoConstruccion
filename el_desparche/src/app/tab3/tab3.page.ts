d0c6f35161fcb0232ef71837f7c67d16876a2e72
Evly <evelyny.guzmanc@konradlorenz.edu.co>
2019-05-23 00:08:58 -0500

    map
    Google maps api is added in tab 3.

--- el_desparche/src/app/tab3/tab3.page.ts
+++ el_desparche/src/app/tab3/tab3.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+declare var google;
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit, AfterContentInit {
+  map;
+  @ViewChild('mapElement') mapElement;
 
   constructor(public geolocation:Geolocation){
 
@@ -21,5 +24,18 @@ export class Tab3Page {
     this.geolocation.getCurrentPosition().then((geposition:Geoposition)=>{
         console.log(geposition)
     })
+  }
+
+  ngOnInit(): void{
+    
+  }
+
+  ngAfterContentInit(): void {
+    this.map = new google.maps.Map(
+        this.mapElement.nativeElement,
+        {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
   }
 }
